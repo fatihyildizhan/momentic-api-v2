85629ea27a56fe464eff951c75d9674a667833ce
fatihyildizhan <fatih@momencraft.com>
2014-11-08 01:25:33 +0200

    values3

--- MomenticAPI/Controllers/ValuesController.cs
+++ MomenticAPI/Controllers/ValuesController.cs
@@ -10,7 +10,7 @@ namespace MomenticAPI.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
