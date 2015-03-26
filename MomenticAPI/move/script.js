3620897af5a1887152ff10be767a19246c2f4e48
Fatih YILDIZHAN <fatih@momencraft.com>
2015-03-26 04:18:11 +0200

    movies

---
+++ MomenticAPI/move/script.js
@@ -0,0 +1,9 @@
+﻿$('#clickme').click(function () {
+    $('#book').animate({
+        opacity: 0.25,
+        left: '+=50',
+        height: 'toggle'
+    }, 5000, function () {
+        // Animation complete.
+    });
+});
