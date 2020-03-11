da70fa432ee2ca0e7c939cc91f928d029f3a024f
Fatih YILDIZHAN <fatih@doxalabs.co.uk>
2020-03-11 22:33:18 +0300

    upgrade to .net framework 4.8

--- MomenticAPI/Controllers/ActivityController.cs
+++ MomenticAPI/Controllers/ActivityController.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
@@ -13,7 +10,7 @@ using MomenticAPI.Models;
 using System.Web.Mvc;
 using System.Dynamic;
 using Newtonsoft.Json;
-using System.Web.OData;
+using System.Web.Http.OData;
 
 namespace MomenticAPI.Controllers
 {
